15caf8edc111b903ad676fb5b00950915f9d3d16
Thomas Sousa <thomsilva084@gmail.com>
2026-01-06 14:25:42 +0000

    erro build vercel corrigido/adiciona frase de ana clara

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
     return <><h1>Oi Naclara, te amo! S2</h1>
-    <h2>Ana Clara: Eu desejo que você consiga alcançar todos os seus objetivos e sonhos, \ne você pode ter certeza de que eu estarei sempre ao seu lado para te apoiar</h2></>
+    <h2>Ana Clara: Eu desejo que você consiga alcançar todos os seus objetivos e sonhos, 
+        e você pode ter certeza de que eu estarei sempre ao seu lado para te apoiar</h2></>
     }
 
 
