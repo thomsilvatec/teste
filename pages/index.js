b2984dd6bd2ba65bff03e8451381f75cb1ff406a
Thomas Sousa <thomsilva084@gmail.com>
2026-01-06 14:17:43 +0000

    adiciona mensagem de ana clara

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi Naclara, te amo! S2</h1>
+    return <><h1>Oi Naclara, te amo! S2</h1><p><br>Ana Clara: Eu desejo que você consiga alcançar todos os seus objetivos e sonhos, e você pode ter certeza de que eu estarei sempre ao seu lado para te apoiar</br></p></>
 }
 
 export default Home;
